314f24d85dd8f24730682bcdb04a11aac85c8d93
Tausif Islam Sheik <mr.tausif904@gmail.com>
2025-06-25 20:17:19 +0600

    Donation section second card calculation completed

--- JS/home.js
+++ JS/home.js
@@ -15,3 +15,22 @@ document.getElementById('card1-btn').addEventListener('click', function(){
     
     document.getElementById('inputField-card1').value ='';
 });
+
+// card 2
+
+document.getElementById('card2-btn').addEventListener('click', function(){
+    const inputValue = cardInputValueById('inputField-card2');
+    if(isNaN(inputValue)){
+        alert('Please provide numeric value');
+        document.getElementById('inputField-card2').value ='';
+        return;
+    }
+    const cardBalance = cardBalanceById('card-2');
+    const cardNewBalance1 = inputValue + cardBalance;
+    document.getElementById('card-2').innerText = cardNewBalance1;
+    const totalBalance = totalBalanceById('total-balance');
+    const totalNewBalance = totalBalance - inputValue;
+    document.getElementById('total-balance').innerText = totalNewBalance;
+    
+    document.getElementById('inputField-card2').value ='';
+});
